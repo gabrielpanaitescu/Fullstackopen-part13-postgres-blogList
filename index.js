469baf927ebeb8c798ca3f3049368d105af02694
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2025-02-25 22:13:34 +0200

    Complete exercise 13.4

--- index.js
+++ index.js
@@ -1,23 +1,91 @@
 const express = require("express");
 require("dotenv").config();
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const app = express();
 app.use(express.json());
 
-const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection successful");
-    await sequelize.close();
-  } catch (error) {
-    console.log("Could not connect to DB");
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "blog",
+    timestamps: false,
   }
-};
+);
 
-main();
+Blog.sync();
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+
+  console.log(JSON.stringify(blogs, null, 2));
+
+  res.json(blogs);
+});
+
+app.get("/api/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+
+  if (blog) {
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  try {
+    await Blog.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).end();
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`App started listening at PORT ${PORT}`);
+});
 
 module.exports = {
   sequelize,
