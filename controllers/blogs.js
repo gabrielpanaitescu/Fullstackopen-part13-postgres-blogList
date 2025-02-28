9bdd9d0e1b8a57d51a01f4f95aca8767f9c58c68
Gabriel Panaitescu <124690179+dez4s@users.noreply.github.com>
2025-02-28 03:28:23 +0200

    Complete exercise 13.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,9 +12,20 @@ blogsRouter.get("/", async (req, res) => {
   const where = {};
 
   if (req.query.search)
-    where.title = {
-      [Op.iLike]: `%${req.query.search}%`,
-    };
+    where[Op.or] = [
+      {
+        title: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+      {
+        author: {
+          [Op.iLike]: `%${req.query.search}%`,
+        },
+      },
+    ];
+
+  console.log("where", where);
 
   const blogs = await Blog.findAll({
     attributes: {
